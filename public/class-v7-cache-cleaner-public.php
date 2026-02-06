4305740222c1e2770754e10092a928611e4597e9
Vaibhaw Kumar <imvaibhaw@gmail.com>
2026-02-06 11:26:43 +0530

    Clean up whitespace in V7_Cache_Cleaner_Public class

--- public/class-v7-cache-cleaner-public.php
+++ public/class-v7-cache-cleaner-public.php
@@ -10,15 +10,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class V7_Cache_Cleaner_Public {
-
 	private $plugin_name;
 	private $version;
-
 	public function __construct( $plugin_name, $version ) {
 		$this->plugin_name = $plugin_name;
 		$this->version     = $version;
 	}
-
 	public function add_browser_cache_headers() {
 		if ( is_admin() || is_user_logged_in() ) {
 			return;
@@ -26,7 +23,6 @@ class V7_Cache_Cleaner_Public {
 
 		$lifetime = (int) get_option( 'v7_cache_lifetime', 24 );
 		$seconds  = $lifetime * 3600;
-
 		header( 'Cache-Control: public, max-age=' . $seconds );
 		header( 'Expires: ' . gmdate( 'D, d M Y H:i:s', time() + $seconds ) . ' GMT' );
 		header( 'Vary: Accept-Encoding' );
